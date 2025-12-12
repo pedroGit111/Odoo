3b4e33ce7ea3729e9d225887a4f9b514977bf65a
pedroGit111 <pedro.martorell@isepceu.es>
2025-12-12 20:26:49 +0100

    Update biblioteca_libro.py

--- extra-addons/biblioteca_alumno/models/biblioteca_libro.py
+++ extra-addons/biblioteca_alumno/models/biblioteca_libro.py
@@ -1,9 +1,10 @@
 from odoo import models, fields
 class BibliotecaLibro(models.Model):
-_name = "biblioteca.libro"
-_description = "Libro de la biblioteca (práctica DAM)"
-name = fields.Char("Título", required=True)
-autor = fields.Char("Autor")
-fecha_publicacion = fields.Date("Fecha de publicación")
-paginas = fields.Integer("Número de páginas")
-disponible = fields.Boolean("Disponible", default=True)
+  _name = "biblioteca.libro"
+  _description = "Libro de la biblioteca (práctica DAM)"
+  name = fields.Char("Título", required=True)
+  autor = fields.Char("Autor")
+  fecha_publicacion = fields.Date("Fecha de publicación")
+  paginas = fields.Integer("Número de páginas")
+  disponible = fields.Boolean("Disponible", default=True)
+  pass
